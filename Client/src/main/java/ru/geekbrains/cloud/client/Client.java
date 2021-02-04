52d231d02e1a26c66ed3af904846765821eeae5f
IMBABOT1 <zlovred12@gmail.com>
2021-02-04 16:58:59 +0300

    dependency

---
+++ Client/src/main/java/ru/geekbrains/cloud/client/Client.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.cloud.client;
+
+
+import ru.geekbrains.cloud.server.Server;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
