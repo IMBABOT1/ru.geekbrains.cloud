15f3a7b4691f6148ec83014a31117eb9c68f1dfb
IMBABOT1 <zlovred12@gmail.com>
2021-02-06 15:48:29 +0300

    network

--- Client/src/main/java/ru/geekbrains/cloud/client/Controller.java
+++ Client/src/main/java/ru/geekbrains/cloud/client/Controller.java
@@ -1,7 +1,5 @@
 package ru.geekbrains.cloud.client;
 
-import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
-import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,7 +13,6 @@ import ru.geekbrains.cloud.common.FileRequest;
 import ru.geekbrains.cloud.common.FileSend;
 
 import java.io.IOException;
-import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -44,10 +41,8 @@ public class Controller implements Initializable {
     @FXML
     Label server;
 
+    public Network network;
 
-    private Socket socket;
-    private ObjectEncoderOutputStream out;
-    private ObjectDecoderInputStream in;
 
     private void moveCloudLabel(){
         cloud.setMaxWidth(Double.MAX_VALUE);
@@ -69,19 +64,19 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        moveLocalLabel();
-        moveCloudLabel();
-        moveServerLabel();
-        try {
-            socket = new Socket("localhost", 8189);
-            in = new ObjectDecoderInputStream(socket.getInputStream());
-            out = new ObjectEncoderOutputStream(socket.getOutputStream());
+            if (network != null && network.isConnected()) {
+                return;
+            }
+            network = new Network(8189);
+            moveLocalLabel();
+            moveCloudLabel();
+            moveServerLabel();
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        AbstractMessage message = (AbstractMessage) in.readObject();
-                        if (message instanceof FileMessage){
+                        AbstractMessage message = (AbstractMessage) network.getIn().readObject();
+                        if (message instanceof FileMessage) {
                             FileMessage fm = (FileMessage) message;
                             Files.write(Paths.get("clientStorage/" + fm.getName()), fm.getData(), StandardOpenOption.CREATE);
                         }
@@ -90,55 +85,24 @@ public class Controller implements Initializable {
                         e.printStackTrace();
                     } catch (IOException e) {
                         e.printStackTrace();
+                    } finally {
+                        network.stop();
                     }
-
                 }
             }).start();
-        }catch (IOException e){
-            throw new RuntimeException("Server is down");
-        }finally {
-           // stop();
         }
-    }
 
-    public void stop(){
-        try {
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public boolean sendMessage(AbstractMessage abstractMessage){
-       try {
-           out.writeObject(abstractMessage);
-           return true;
-       }catch (IOException e){
-           e.printStackTrace();
-       }
-
-       return false;
-    }
 
 
 
     public void clientDownload(ActionEvent actionEvent) {
-        sendMessage(new FileRequest("12.txt"));
+        network.sendMessage(new FileRequest("12.txt"));
     }
 
     public void clientUpload(ActionEvent actionEvent) {
         FileSend fileSend = new FileSend(Paths.get("clientStorage/" + "rogueLike.rar"));
-        sendMessage(fileSend);
+        network.sendMessage(fileSend);
     }
 
 
--- Client/src/main/java/ru/geekbrains/cloud/client/Network.java
+++ Client/src/main/java/ru/geekbrains/cloud/client/Network.java
@@ -1,6 +1,78 @@
 package ru.geekbrains.cloud.client;
 
+import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
+import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
+import ru.geekbrains.cloud.common.AbstractMessage;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Network {
 
+    private Socket socket;
 
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public ObjectEncoderOutputStream getOut() {
+        return out;
+    }
+
+    public ObjectDecoderInputStream getIn() {
+        return in;
+    }
+
+    private ObjectEncoderOutputStream out;
+    private ObjectDecoderInputStream in;
+
+
+    public Network(int port){
+        try {
+            socket = new Socket("localhost", port);
+            in = new ObjectDecoderInputStream(socket.getInputStream());
+            out = new ObjectEncoderOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean sendMessage(AbstractMessage abstractMessage){
+        try {
+            out.writeObject(abstractMessage);
+            return true;
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+
+    public boolean isConnected(){
+        if (socket == null || socket.isClosed()){
+            return false;
+        }
+        return true;
+    }
+
+
+    public void stop(){
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
